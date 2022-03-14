858a88644bcc5fa45214f9f1851ad26f16f9bda1
ParthG-Gulati <gulatiparth2001@gmail.com>
2022-03-14 11:11:23 +0530

    [UPDT] Added Button in appointment to move to hospitalization form

--- my_hospital_management/models/appointment.py
+++ my_hospital_management/models/appointment.py
@@ -8,3 +8,13 @@ class HospitalAppointmentInherited(models.Model):
     doctor_app = fields.Many2one('hr.employee', string='Doctor Name')
     hospitalization_selection = fields.Boolean(string='Hospitalize')
 
+    def action_hospitalize(self):
+        return{
+            'res_model': 'sale.order',
+            'type': 'ir.actions.act_window',
+            'view_mode': 'form',
+            'view_type': 'form',
+            'view_id': self.env.ref("sale.view_order_form").id,
+            'target': 'self'
+        }
+
